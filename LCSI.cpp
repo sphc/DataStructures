aad461c4702b573c1e612813dc94e0a1f2a5c019
sphc <jinkai0916@outlook.com>
2020-12-15 13:15:18 +0800

    refractory with cpp20

--- LCSI.cpp
+++ LCSI.cpp
@@ -1,27 +1,52 @@
-//C++ file
+/*
+ * @Author       : sphc
+ * @Description  : ---
+ * @Email        : jinkai0916@outlook.com
+ * @Date         : 2020-08-11 12:06:12
+ * @LastEditors  : sphc
+ * @LastEditTime : 2020-12-15 13:13:54
+ */
 
-//================================================
-//
-//      Filename: LCSI.cpp
-//
-//        Author: sphc - jinkai0916@outlook.com
-//   Description: ---
-//       Created: 2020-08-11 12:06:12
-// Last Modified: 2020-08-11 12:17:49
-//
-//================================================
-
+#include <span>
+#include <array>
 #include <iostream>
 
+// //求A、B最长公共子序列的长度
+// int LCSI(const char *A, int lenA, const char *B, int lenB)
+// {
+//     int dp[100][100];
+//     for (int i = 0; i < 100; ++i) {
+//         dp[i][0] = dp[0][i] = 0;
+//     }
+//     for (int i = 1; i <= lenA; ++i) {
+//         for (int j = 1; j <= lenB; ++j) {
+//             if (A[i - 1] == B[j - 1]) {
+//                 dp[i][j] = dp[i - 1][ j - 1] + 1;
+//             } else {
+//                 dp[i][j] = dp[i - 1][j] < dp[i][j - 1] ? dp[i][j - 1] : dp[i - 1][j];
+//             }
+//         }
+//     }
+//     return dp[lenA][lenB];
+// }
+
+// int main()
+// {
+//     char a[] = "asymptotic";
+//     char b[] = "spot";
+//     std::cout << LCSI(a, 10, b, 4) << std::endl;
+//     return 0;
+// }
+
 //求A、B最长公共子序列的长度
-int LCSI(const char *A, int lenA, const char *B, int lenB)
+int LCSI(std::span<const char> A, std::span<const char> B)
 {
-    int dp[100][100];
-    for (int i = 0; i < 100; ++i) {
+    std::array<std::array<int, 100>, 100> dp;
+    for (decltype(dp.size()) i{0}; i < dp.size(); ++i) {
         dp[i][0] = dp[0][i] = 0;
     }
-    for (int i = 1; i <= lenA; ++i) {
-        for (int j = 1; j <= lenB; ++j) {
+    for (decltype(A.size()) i{1}; i <= A.size(); ++i) {
+        for (decltype(B.size()) j{1}; j <= B.size(); ++j) {
             if (A[i - 1] == B[j - 1]) {
                 dp[i][j] = dp[i - 1][ j - 1] + 1;
             } else {
@@ -29,13 +54,15 @@ int LCSI(const char *A, int lenA, const char *B, int lenB)
             }
         }
     }
-    return dp[lenA][lenB];
+    return dp[A.size()][B.size()];
 }
 
 int main()
 {
-    char a[] = "asymptotic";
-    char b[] = "spot";
-    std::cout << LCSI(a, 10, b, 4) << std::endl;
+    char a[]{"asymptotic"};
+    char b[]{"spot"};
+    std::span<const char> aspan{a};
+    std::span<const char> bspan{b};
+    std::cout << LCSI(aspan.subspan(0, aspan.size() - 1), bspan.subspan(0, bspan.size() - 1)) << std::endl;
     return 0;
 }
--- LCSR.cpp
+++ LCSR.cpp
@@ -1,37 +1,62 @@
+/*
+ * @Author       : sphc
+ * @Description  : ---
+ * @Email        : jinkai0916@outlook.com
+ * @Date         : 2020-09-09 23:51:13
+ * @LastEditors  : sphc
+ * @LastEditTime : 2020-12-15 13:14:24
+ */
 //C++ file
 
-//================================================
-//
-//      Filename: LCSR.cpp
-//
-//        Author: sphc - jinkai0916@outlook.com
-//   Description: ---
-//       Created: 2020-08-11 11:07:23
-// Last Modified: 2020-08-11 12:06:01
-//
-//================================================
-
+#include <span>
 #include <iostream>
 
+// //求A、B最长公共子序列的长度
+// int LCSR(const char *A, int lenA, const char *B, int lenB)
+// {
+//     if (lenA < 1 || lenB < 1) {
+//         return 0;
+//     }
+//     if (A[lenA - 1] == B[lenB - 1]) {
+//         return LCSR(A, lenA - 1, B, lenB - 1) + 1;
+//     } else {
+//         int ret1 = LCSR(A, lenA - 1, B, lenB);
+//         int ret2 = LCSR(A, lenA, B, lenB - 1);
+//         return ret1 < ret2 ? ret2 : ret1;
+//     }
+// }
+
+// int main()
+// {
+//     char a[] = "asymptotic";
+//     char b[] = "spot";
+//     std::cout << LCSR(a, 10, b, 4) << std::endl;
+//     return 0;
+// }
+
 //求A、B最长公共子序列的长度
-int LCSR(const char *A, int lenA, const char *B, int lenB)
+int LCSR(std::span<const char> A, std::span<const char> B)
 {
-    if (lenA < 1 || lenB < 1) {
-        return 0;
-    }
+    auto lenA = A.size();
+    auto lenB = B.size();
+    if (lenA < 1 || lenB < 1) { return 0; }
+    auto subA = A.subspan(0, lenA - 1);
+    auto subB = B.subspan(0, lenB - 1);
     if (A[lenA - 1] == B[lenB - 1]) {
-        return LCSR(A, lenA - 1, B, lenB - 1) + 1;
+        return LCSR(subA, subB) + 1;
     } else {
-        int ret1 = LCSR(A, lenA - 1, B, lenB);
-        int ret2 = LCSR(A, lenA, B, lenB - 1);
+        int ret1{LCSR(subA, B)};
+        int ret2{LCSR(A, subB)};
         return ret1 < ret2 ? ret2 : ret1;
     }
 }
 
 int main()
 {
-    char a[] = "asymptotic";
-    char b[] = "spot";
-    std::cout << LCSR(a, 10, b, 4) << std::endl;
+    char a[]{"asymptotic"};
+    char b[]{"spot"};
+    std::span<const char> aspan{a};
+    std::span<const char> bspan{b};
+    std::cout << LCSR(aspan.subspan(0, aspan.size() - 1), bspan.subspan(0, bspan.size() - 1)) << std::endl;
     return 0;
 }
